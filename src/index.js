bdf3846866eca2a5aaa922ee382f1829b3ce71d3
Eduardo Elias Alves Pereira <eduardo.eliasap@gmail.com>
2020-11-04 14:25:12 -0300

    Add list and delete posts

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/upload', {
+mongoose.connect(process.env.MONGO_URL, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
@@ -14,6 +15,7 @@ mongoose.connect('mongodb://localhost:27017/upload', {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use('/files', express.static(path.resolve(__dirname, '..', '..', 'tmp', 'uploads')));
 
 app.use(require("./routes"));
 
--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,21 @@ routes.get("/", (req, res) => {
   return res.json({ ok: true });
 })
 
-routes.post("/", multer(multerConfig).single('file'), (req, res) => {
+routes.get("/posts", async (req, res) => {  
+  const posts = await Post.find();
+
+  return res.json(posts);
+});
+
+routes.delete("/posts/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  await post.remove();
+
+  return res.send();
+});
+
+routes.post("/posts", multer(multerConfig).single('file'), (req, res) => {
   console.log(req.file); // This is the multer return
 
   const { originalname: name, size, key, location: url = '' } = req.file;
